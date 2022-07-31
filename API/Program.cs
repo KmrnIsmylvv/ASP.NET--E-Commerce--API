44dc95c650084b89002c63e2897e773cffeade81
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-07-31 21:34:26 +0400

    feat: Added automatic Seeding Data

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,16 +14,31 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+                ILoggerFactory loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    StoreContext context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public  class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    string brandsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/brands.json");
+
+                    List<ProductBrand> brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach (ProductBrand brand in brands)
+                    {
+                        context.ProductBrands.Add(brand);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    string typesData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/types.json");
+
+                    List<ProductType> types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach (ProductType type in types)
+                    {
+                        context.ProductTypes.Add(type);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    string productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                    List<Product> products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    foreach (Product product in products)
+                    {
+                        context.Products.Add(product);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ILogger<StoreContextSeed> logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
