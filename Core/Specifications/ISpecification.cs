1342f7cbf2cbaf04e22d49a17e47df5ffd77f381
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-08-24 14:47:01 +0400

    feat: Added Specification class

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 namespace Core.Specifications
 {
     public interface ISpecification<T>
-    {
+    { 
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
     }
