bfef8927eb5995e2df37d10e32ff24d65fff50c2
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-08-25 15:37:10 +0400

    feat: Added SpecificationEvaluator class

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery,
+            ISpecification<TEntity> spec)
+        {
+            IQueryable<TEntity> query = inputQuery;
+
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(
+                query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
