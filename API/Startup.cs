a1b30035d04e32c75e576d12e925eb984c2234e9
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-09-08 15:21:57 +0400

    feat: Serving static content from the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -51,6 +51,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
