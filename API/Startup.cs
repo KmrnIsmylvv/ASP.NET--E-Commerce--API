16e87217c6cb7bb347e05ae2f9a1d5ee1293ab5a
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-09-15 16:05:32 +0400

    feat: Added ApiBehaviourOptions configuration into startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using API.Errors;
 using API.Helpers;
 using API.Middleware;
 using Core.Interfaces;
@@ -6,6 +8,7 @@ using Infrastructure.Data;
 using Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -36,13 +39,31 @@ namespace API
 
             services.AddDbContext<StoreContext>(opt =>
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    string[] errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
+
+                    ApiValidationErrorResponse errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            
+
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
