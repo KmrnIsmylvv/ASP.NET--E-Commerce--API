94d2d00c8c15cff666f14ca4de89f60e697d9b92
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-09-15 16:04:00 +0400

    feat: Created ApiValidationErrorResponse class

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace API.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+
+    {
+        public ApiValidationErrorResponse() : base(400)
+        {
+        }
+
+        public IEnumerable<string> Errors { get; set; }
+        
+    }
+}
