bf417a9cbe2778c2c49de0f7e168d96c10bcc5b3
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-09-24 15:38:38 +0400

    refactor: update SpecificationEvaluator class

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -17,6 +17,17 @@ namespace Infrastructure.Data
                 query = query.Where(spec.Criteria);
             }
 
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+
             query = spec.Includes.Aggregate(
                 query, (current, include) => current.Include(include));
 
