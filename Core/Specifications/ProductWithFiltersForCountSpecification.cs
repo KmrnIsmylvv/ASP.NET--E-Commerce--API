86e99e24b0cede974518ca71ad80172e7ab507b3
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-10-02 17:01:07 +0400

    feat: Created new Specification class for Pagination Count

---
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
+    {
+        public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
+            : base(x =>
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
+        {
+        }
+    }
+}
