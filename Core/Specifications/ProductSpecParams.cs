d437c069c5f39cd38432c650ef7c92617bbcb3d5
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-10-04 16:28:49 +0400

    feat: Added Search Functionality

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -17,5 +17,13 @@
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
         public string Sort { get; set; }
+
+        private string _search;
+
+        public string Search
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
     }
 }
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -6,9 +6,10 @@ namespace Core.Specifications
     {
         public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
             : base(x =>
-                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
-                (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
-            )
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search) &&
+                    (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                    (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+                ))
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
