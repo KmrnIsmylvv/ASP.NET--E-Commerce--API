c09a7da1f27505585b30b12afb60f32a9c201db0
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-10-14 23:14:37 +0400

    feat: Create Product Interfaces in client side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {IProduct} from "./models/product";
+import {IPagination} from "./models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,14 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[] = [];
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products').subscribe((response: any) => {
-      console.log(response);
+      this.products = response.data;
     }, error => {
       console.log(error);
     })
