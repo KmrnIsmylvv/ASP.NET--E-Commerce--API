b7f4cefe9aa4b169884eecb29e70f815313f5403
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-10-15 17:25:52 +0400

    feat: Added new modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,21 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
-import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
