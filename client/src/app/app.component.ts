457bff0289b4edf3989f4262797ce07651c51a45
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-11 17:18:22 +0400

    feat: Added getProducts into Shop Service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
   products: IProduct[] = [];
 
   constructor(private http: HttpClient) {
+
   }
 
   ngOnInit(): void {
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IPagination} from "../models/pagination";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = "https://localhost:5001/api/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(){
+      return this.http.get<IPagination>(this.baseUrl+ 'products');
+  }
 }
