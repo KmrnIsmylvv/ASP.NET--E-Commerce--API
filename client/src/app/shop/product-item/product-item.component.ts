dc710fac5cf43ad067ea7be5c9d538618381a51e
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-17 15:40:58 +0400

    feat: Passing down data to child components

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {IProduct} from "../../models/product";
 
 @Component({
   selector: 'app-product-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
+  @Input() product!: IProduct ;
+
 
   constructor() { }
 
