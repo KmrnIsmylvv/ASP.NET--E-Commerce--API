0dd465e2073477d79aacf31906a4f050b34f1d26
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-11-17 14:55:03 +0400

    feat: Adding a child component for the product items

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule
