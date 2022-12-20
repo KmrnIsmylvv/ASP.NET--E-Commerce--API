9beed4b3d9a0873e2be78d2ffc592884da3e634d
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-12-20 17:41:45 +0400

    feat: Created Basket class

---
+++ Core/Entities/BasketItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public string PictureUrl { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+    }
+}
