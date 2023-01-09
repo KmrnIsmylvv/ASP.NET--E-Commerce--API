491440333ad1a213aa50adee422a47e0ca6c7fa2
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-09 22:28:39 +0400

    feat: Registering User

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
