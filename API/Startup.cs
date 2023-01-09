8bbbbf9d82e5a6771b6e0eaf14ae8a50f7664d9b
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-09 23:11:28 +0400

    feat: Setting up identity to use the token

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,18 +1,11 @@
-using System.Linq;
-using API.Errors;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
-using Core.Interfaces;
-using Infrastructure.Data;
-using Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using StackExchange.Redis;
 
 namespace API
@@ -35,7 +28,7 @@ namespace API
 
             services.AddApplicationServices(_config);
             services.AddSwaggerDocumentation();
-            services.AddIdentityService();
+            services.AddIdentityService(_config);
 
             services.AddSingleton<IConnectionMultiplexer>(c =>
             {
@@ -71,6 +64,7 @@ namespace API
 
             app.AddSwaggerUI();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
