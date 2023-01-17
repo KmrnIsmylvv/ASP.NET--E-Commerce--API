608394d3fa1b4b7674b5a3f87a24080608646e6a
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-17 16:53:39 +0400

    feat: Added email and password validations

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class RegisterDto
     {
-        public string DisplayName { get; set; }
-        public string Email { get; set; }
+        [Required] public string DisplayName { get; set; }
+        [Required] [EmailAddress] public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(
+            "(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage =
+                "Password must have 1 uppercase, 1 lowercase, 1 number, 1 non alphanumeric and at least 6 character")]
         public string Password { get; set; }
     }
 }
