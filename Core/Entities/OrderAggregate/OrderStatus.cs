224084d8f2df3241c8440771a4eacc5404b74651
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-18 14:41:04 +0400

    feat: Added new classes and enum for Order

---
+++ Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Entities.OrderAggregate;
+
+public enum OrderStatus
+{
+    [EnumMember(Value = "Pending")]
+    Pending,
+    [EnumMember(Value = "PaymentReceived")]
+    PaymentReceived,
+    [EnumMember(Value = "Payment Failed")]
+    PaymentFailed
+}
