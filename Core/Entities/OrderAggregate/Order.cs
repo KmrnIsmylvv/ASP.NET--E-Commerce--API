c6b73773b520870e387aea0d181900931791df8e
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-19 22:04:18 +0400

    fsat: Complete Order Aggregate section

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Entities.OrderAggregate;
+
+public class Order : BaseEntity
+{
+    public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress,
+        DeliveryMethod deliveryMethod, decimal subtotal)
+    {
+        BuyerEmail = buyerEmail;
+        ShipToAddress = shipToAddress;
+        DeliveryMethod = deliveryMethod;
+        OrderItems = orderItems;
+        Subtotal = subtotal;
+    }
+
+    public string BuyerEmail { get; set; }
+    public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+    public Address ShipToAddress { get; set; }
+    public DeliveryMethod DeliveryMethod { get; set; }
+    public IReadOnlyList<OrderItem> OrderItems { get; set; }
+    public decimal Subtotal { get; set; }
+    public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+    public int PaymentIntentId { get; set; }
+
+    public decimal GetTotal()
+    {
+        return Subtotal + DeliveryMethod.Price;
+    }
+}
