ff3474c15b4ddf558a58d1ef8d4bd912439660f9
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-01-21 23:10:39 +0400

    refactor: Store context update

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
 using System.Reflection;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using StackExchange.Redis;
+using Order = Core.Entities.OrderAggregate.Order;
 
 namespace Infrastructure.Data
 {
@@ -15,6 +18,9 @@ namespace Infrastructure.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.Data
@@ -52,6 +53,20 @@ namespace Infrastructure.Data
                     foreach (Product product in products)
                     {
                         context.Products.Add(product);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+                
+                if (!context.DeliveryMethods.Any())
+                {
+                    string dmData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/delivery.json");
+
+                    var methods = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
+
+                    foreach (var method in methods)
+                    {
+                        context.DeliveryMethods.Add(method);
                     }
 
                     await context.SaveChangesAsync();
