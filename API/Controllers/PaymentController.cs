6b34cf9442698efb37880de2cc4e89198fbf5a7f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2023-02-10 12:53:57 +0400

    feat: Creating the payment controller

---
+++ API/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class PaymentController : BaseApiController
+{
+    private readonly IPaymentService _paymentService;
+
+    public PaymentController(IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+    }
+
+    [Authorize]
+    [HttpPost("{basketId}")]
+    public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+    {
+        return await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+    }
+}
--- API/DTOs/CustomerBasketDto.cs
+++ API/DTOs/CustomerBasketDto.cs
@@ -5,7 +5,9 @@ namespace API.DTOs;
 
 public class CustomerBasketDto
 {
-    [Required]
-    public string Id { get; set; }
+    [Required] public string Id { get; set; }
     public List<BasketItemDto> Items { get; set; }
+    public int? DeliveryMethodId { get; set; }
+    public string ClientSecret { get; set; }
+    public string PaymentIntentId { get; set; }
 }
